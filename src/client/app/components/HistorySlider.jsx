9c43d7db240aff2351f52756eb2d6f6461791f6e
athomschke <astrid.thomschke@gmail.com>
2016-05-19 14:33:13 +0200

    Remove History Slider as it will distort overview over tests written in #4

--- src/client/app/components/HistorySlider.jsx
+++
@@ -1,6 +0,0 @@
-import ReactSlider from 'react-slider';	
-import React from 'react';
-
-export default class HistorySlider extends ReactSlider {
-
-}
--- test/components/HistorySlider.spec.jsx
+++
@@ -1,9 +0,0 @@
-import HistorySlider from 'components/HistorySlider';
-import TestUtils from 'react-addons-test-utils';
-import React from 'react';
-
-describe('HistorySlider', () => {
-	it('can be created', () => {
-		TestUtils.renderIntoDocument(<HistorySlider></HistorySlider>)
-	})
-})
