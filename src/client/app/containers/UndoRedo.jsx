d35a88a8b4d6dd93c742c61a8fd0a358cdb8457d
athomschke <astrid.thomschke@gmail.com>
2016-05-20 10:17:58 +0200

    Implement dumb version of history slider for #1

--- src/client/app/containers/UndoRedo.jsx
+++ src/client/app/containers/UndoRedo.jsx
@@ -1,29 +1,53 @@
-import { ActionCreators as UndoActionCreators } from 'redux-undo';
+import { ActionCreators } from 'redux-undo';
 import { connect } from 'react-redux';
 import React from 'react';
+import ReactSlider from 'react-slider';
 
-let UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
-  <p>
+let currentValue;
+
+let UndoRedo = ({ canUndo, canRedo, max, value, onUndo, onRedo, onChange}) => (
+  <div>
     <button onClick={onUndo} disabled={!canUndo}>
       Undo
     </button>
+    <ReactSlider
+      type="range"
+      disabled={!canUndo && !canRedo}
+      min={0}
+      max={max}
+      onChange={onChange}
+      value={value}
+    ></ReactSlider>
     <button onClick={onRedo} disabled={!canRedo}>
       Redo
     </button>
-  </p>
+  </div>
 )
-
+ 
 const mapStateToProps = (state) => {
+  currentValue = state.scene.past.length;
   return {
     canUndo: state.scene.past.length > 0,
-    canRedo: state.scene.future.length > 0
+    canRedo: state.scene.future.length > 0,
+    max: state.scene.past.length + state.scene.future.length,
+    value: state.scene.past.length
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onUndo: () => dispatch(UndoActionCreators.undo()),
-    onRedo: () => dispatch(UndoActionCreators.redo())
+    onUndo: () => dispatch(ActionCreators.undo()),
+    onRedo: () => dispatch(ActionCreators.redo()),
+    onChange: (newValue) => {
+      let oldValue = currentValue;
+      if (newValue < oldValue) {
+        debugger
+        return dispatch(ActionCreators.jumpToPast(newValue))
+      } else if (newValue > oldValue) {
+        debugger
+        return dispatch(ActionCreators.jumpToFuture(newValue - oldValue - 1))
+      }
+    }
   }
 }
 
