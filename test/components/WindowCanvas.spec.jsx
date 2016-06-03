7a2051f8406253dfad9e969f2fd120581efd2eb5
athomschke <astrid.thomschke@gmail.com>
2016-06-03 11:06:10 +0200

    Test resizing for #6

--- test/components/WindowCanvas.spec.jsx
+++ test/components/WindowCanvas.spec.jsx
@@ -11,10 +11,32 @@ let renderCanvasWithStrokes = (strokes) => {
 
 describe('WindowCanvas', () => {
 
+	let restorableWidth;
+	let restorableHeight;
+	let windowCanvas;
+
+	beforeEach(() => {
+		restorableWidth = window.innerWidth;
+		restorableHeight = window.innerHeight;
+		windowCanvas = renderCanvasWithStrokes([]);
+	})
+
+	afterEach(() => {
+		window.innerWidth = restorableWidth;
+		window.innerHeight = restorableHeight;
+	})
+
 	it('has the size of the window', () => {
-		let windowCanvas = renderCanvasWithStrokes([]);
 		expect(windowCanvas.state.windowWidth).to.equal(window.innerWidth);
 		expect(windowCanvas.state.windowHeight).to.equal(window.innerHeight);
+	})
+
+	it('resizes with the window', () => {
+		window.innerWidth = 100;
+		window.innerHeight = 100;
+		windowCanvas.handleResize();
+		expect(windowCanvas.state.windowWidth).to.equal(100);
+		expect(windowCanvas.state.windowHeight).to.equal(100);
 	})
 
 })
