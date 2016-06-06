2d06bfa7c1ed461793470ce09a31f5d7e7d627d8
athomschke <astrid.thomschke@gmail.com>
2016-06-06 17:27:12 +0200

    Refactor sketch reducer

--- src/client/app/reducers/sketches.js
+++ src/client/app/reducers/sketches.js
@@ -2,26 +2,31 @@ import strokes from 'reducers/strokes';
 import * as actionTypes from 'constants/actionTypes';
 import { last, without } from 'lodash';
 
+let reduceFirstSketch = (state, action) => {
+	return [{
+		strokes: strokes([], action)
+	}];
+}
+
+let reduceMultipleSketches = (state, action) => {
+	let head = without(state, last(state));
+	let tail = {
+		strokes: strokes(last(state).strokes, action)
+	}
+	return head.concat([tail])
+}
+
 const sketches = (state = [], action) => {
 	switch(action.type) {
 		case actionTypes.APPEND_POINT:
-		case actionTypes.CREATE_STROKE:
 		case actionTypes.FINISH_STROKE:
-			if (state.length > 1) {
-				let head = without(state, last(state));
-				let tail = {
-					strokes: strokes(last(state).strokes, action)
-				}
-				return head.concat([tail])
-			} else if (state.length > 0) {
-				return [{
-					strokes: strokes(last(state).strokes, action)
-				}]
-			} else {
-				return [{
-					strokes: strokes([], action)
-				}];
-			}
+			return state.length > 0 ? reduceMultipleSketches(state, action) :
+				reduceFirstSketch(state, action);
+			reduceMultipleSketches();
+		case actionTypes.CREATE_STROKE:
+			return reduceMultipleSketches(state.concat({
+				strokes: strokes([], action)
+			}), action)
 		default:
 			return state;
 	}
