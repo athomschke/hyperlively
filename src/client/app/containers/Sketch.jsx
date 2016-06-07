433778439ed0f5236bc416b63acacedd4aead740
athomschke <astrid.thomschke@gmail.com>
2016-06-07 12:21:52 +0200

    refactor sketch container in order to split it in sketch and scene #9

--- src/client/app/containers/Sketch.jsx
+++ src/client/app/containers/Sketch.jsx
@@ -4,15 +4,12 @@ import { appendPoint, createStroke, finishStroke } from 'actions/drawing';
 import { last } from 'lodash';
 
 const mapStateToProps = (state) => {
-  let strokes = [];
-  if (state.scene.present.sketches.length > 0 && last(state.scene.present.sketches).strokes.length > 0 ) {
-    strokes = last(state.scene.present.sketches).strokes
-  }
-  return {
-    strokes: strokes,
-    usePloma: state.ploma.usePloma,
-    uniqueCanvasFactor: state.ploma.uniqueCanvasFactor
-  }
+  let strokes = state.scene.present.sketches.length > 0 ?
+    last(state.scene.present.sketches).strokes :
+    [];
+  let returnState = _.cloneDeep(state.ploma);
+  returnState.strokes = strokes;
+  return returnState;
 }
 
 const mapDispatchToProps = (dispatch) => {
