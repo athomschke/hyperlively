f571ce7ad305b45231df2b78de9bd806d6b5deda
athomschke <astrid.thomschke@gmail.com>
2016-06-30 13:56:42 +0200

    refactor reducer tests for sketches #9

--- test/reducers/sketches.spec.js
+++ test/reducers/sketches.spec.js
@@ -1,4 +1,5 @@
 import sketches from 'reducers/sketches'
+import { appendPoint, createStroke, finishStroke } from 'actions/drawing'
 import * as types from 'constants/actionTypes'
 import { point } from '../helpers'
 
@@ -18,42 +19,37 @@ describe('sketches', () => {
 
 	describe('adding a point', () => {
 
-		it('when no sketch exists creates a sketch', () => {
-			expect(
-				sketches([], {
-					type: types.APPEND_POINT,
-					point: point(10,10)
-				})
-			).to.deep.equal(
-				[{
-					strokes: [{
-						points: [point(10,10)]
-					}],
-					position: point(10,10)
-				}]
-			)
+		it('to an empty canvas creates a sketch', () => {
+			let result = sketches([], appendPoint(point(10,10)));
+			expect(result).to.have.length(1);
 		})
 
-		it('appends a point always to the last sketch', () => {
+		it('sets the position to the first point', () => {
+			let result = sketches([], appendPoint(point(10,10)));
+			expect(result).to.have.length(1);
+			expect(result[0].position).to.deep.equal(point(10,10));
+		})
+
+		it('appends a point to the last sketch', () => {
+			let result = sketches([], appendPoint(point(10,10)));
+			expect(result[0].position).to.deep.equal(point(10,10));
+		})
+
+		it('cannot create a new sketch if one exists', () => {
 			let currentSketches = [{
-				strokes: [{
-					points: [point(10,10)]
-				}],
+				strokes: []
+			}]
+			let result = sketches(currentSketches, appendPoint(point(10,11)));
+			expect(result).to.have.length(1)
+		})
+
+		it('keeps the original position', () => {
+			let currentSketches = [{
+				strokes: [],
 				position: point(10,10)
 			}]
-			expect(
-				sketches(currentSketches, {
-					type: types.APPEND_POINT,
-					point: point(10,11)
-				})
-			).to.deep.equal(
-				[{
-					strokes: [{
-						points: [point(10,10), point(10,11)]
-					}],
-					position: point(10,10)
-				}]
-			)
+			let result = sketches(currentSketches, appendPoint(point(10,11)));
+			expect(result[0].position).to.deep.equal(point(10,10));
 		})
 
 	})
@@ -61,81 +57,43 @@ describe('sketches', () => {
 	describe('starting a stroke', () => {
 
 		it('when no sketch exists creates a sketch', () => {
-			expect(
-				sketches([], {
-					type: types.CREATE_STROKE,
-					point: point(10,10)
-				})
-			).to.deep.equal(
-				[{
-					strokes: [{
-						points: [point(10,10)]
-					}],
-					position: point(10,10)
-				}]
-			)
+			let result = sketches(
+				[],
+				createStroke(point(10,10))
+			);
+			expect(result).to.have.length(1)
 		})
 
-		it('appends a stroke to the only sketch', () => {
-			let currentSketches = [{
-				strokes: [{
-					points: [point(10,10)]
-				}],
-				position: point(10,10)
-			}]
-			let actualResult = sketches(currentSketches, {
-				type: types.CREATE_STROKE,
-				point: point(10,11)
-			})
-			let expectedResult = [{
-				strokes: [{
-					points: [point(10,10)]
-				}, {
-					points: [point(10,11)]
-				}],
-				position: point(10,10)
-			}]
-			expect(
-				actualResult
-			).to.deep.equal(
-				expectedResult
-			)
+		it('initializes a sketch at the given position', () => {
+			let result = sketches(
+				[],
+				createStroke(point(10,10))
+			);
+			expect(result[0].position).to.deep.equal(point(10,10));
+		})
+
+		it('does not increase the number of sketches if there is only one sketch', () => {
+			let result = sketches(
+				[{ strokes: [] }],
+				createStroke(point(10,10))
+			);
+			expect(result).to.have.length(1);
+		})
+
+		it('does not increase the number of sketches if there are multiple sketches', () => {
+			let result = sketches(
+				[{ strokes: [] }],
+				createStroke(point(10,10))
+			);
+			expect(result).to.have.length(1);
 		})
 
 		it('appends a stroke to the last sketch', () => {
-			let currentSketches = [{
-				strokes: [{
-					points: [ point(10,10) ]
-				}],
-				position: point(10,10)
-			}, {
-				strokes: [{
-					points: [ point(10,10) ]
-				}],
-				position: point(10,10)
-			}]
-			let actionCreator = {
-				type: types.CREATE_STROKE,
-				point: point(10,11)
-			}
-			let extendedSketches = [{
-				strokes: [{
-					points: [ point(10,10) ]
-				}],
-				position: point(10,10)
-			}, {
-				strokes: [{
-					points: [ point(10,10) ]
-				}, {
-					points: [ point(10,11) ]
-				}],
-				position: point(10,10)
-			}]
-			expect(
-				sketches(currentSketches, actionCreator)
-			).to.deep.equal(
-				extendedSketches
-			)
+			let result = sketches(
+				[{ strokes: [] }, { strokes: [] }],
+				createStroke(point(10,10))
+			);
+			expect(result).to.have.length(2);
 		})
 
 	})
@@ -143,65 +101,19 @@ describe('sketches', () => {
 	describe('finishing a stroke', () => {
 
 		it('marks the only sketch as finished', () => {
-			let currentSketches = [{
-				strokes: [{
-					points: [point(10,10), point(10,11)]
-				}],
-				position: point(10,10)
-			}]
-			let actualResult = sketches(currentSketches, {
-				type: types.FINISH_STROKE,
-				point: point(10,12)
-			})
-			let expectedResult = [{
-				strokes: [{
-					points: [point(10,10), point(10,11), point(10,12)],
-					finished: true
-				}],
-				finished: true,
-				position: point(10,10)
-			}]
-			expect(
-				actualResult
-			).to.deep.equal(
-				expectedResult
-			)
+			let result = sketches(
+				[{ strokes: [] }],
+				finishStroke(point(10,10))
+			);
+			expect(result[0].finished).to.be.true
 		})
 
 		it('marks the last sketch as finished', () => {
-			let currentSketches = [{
-				strokes: [{
-					points: [point(10,10), point(10,11)]
-				}],
-				position: point(10,10)
-			}, {
-				strokes: [{
-					points: [point(10,10), point(10,11)]
-				}],
-				position: point(10,10)
-			}]
-			let actualResult = sketches(currentSketches, {
-				type: types.FINISH_STROKE,
-				point: point(10,12)
-			})
-			let expectedResult = [{
-				strokes: [{
-					points: [point(10,10), point(10,11)]
-				}],
-				position: point(10,10)
-			}, {
-				strokes: [{
-					points: [point(10,10), point(10,11), point(10,12)],
-					finished: true
-				}],
-				finished: true,
-				position: point(10,10)
-			}]
-			expect(
-				actualResult
-			).to.deep.equal(
-				expectedResult
-			)
+			let result = sketches(
+				[{ strokes: [] }, { strokes: [] }],
+				finishStroke(point(10,10))
+			);
+			expect(result[1].finished).to.be.true
 		})
 
 	})
