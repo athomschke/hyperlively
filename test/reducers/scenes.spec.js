f068d1fd021c2f2b0e6d2ce8343e3a53dde95e6a
Astrid Thomschke <astrid.thomschke@gmail.com>
2016-11-17 13:19:34 +0100

    add test for updating position to scenes reducer #42

--- test/reducers/scenes.spec.js
+++ test/reducers/scenes.spec.js
@@ -1,5 +1,6 @@
 import { scenes } from 'reducers/scenes';
 import { appendPoint, createStroke } from 'actions/drawing';
+import { updatePosition } from 'actions/manipulating';
 import { point } from '../helpers';
 
 describe('scenes', () => {
@@ -26,7 +27,9 @@ describe('scenes', () => {
 		it('always adds to the current scene', () => {
 			let aPoint = point(10,10);
 			let result = scenes(
-				[{ strokes:[] }],
+				[
+					{ strokes:[] }
+				],
 				createStroke(aPoint.x, aPoint.y, aPoint.timeStamp, 0)
 			);
 			expect(result).to.have.length(1);
@@ -39,7 +42,10 @@ describe('scenes', () => {
 		it('adds a stroke to the second scene', () => {
 			let aPoint = point(10,10);
 			let result = scenes(
-				[{ strokes:[] }, { strokes:[] }],
+				[
+					{ strokes:[] },
+					{ strokes:[] }
+				],
 				createStroke(aPoint.x, aPoint.y, aPoint.timeStamp, 1)
 			);
 			expect(result).to.have.length(2);
@@ -49,11 +55,32 @@ describe('scenes', () => {
 		it('does not a stroke to the first scene', () => {
 			let aPoint = point(10,10);
 			let result = scenes(
-				[{ strokes:[] }, { strokes:[] }],
+				[
+					{ strokes:[] },
+					{ strokes:[] }
+				],
 				createStroke(aPoint.x, aPoint.y, aPoint.timeStamp, 1)
 			);
 			expect(result).to.have.length(2);
 			expect(result[0].strokes).to.have.length(0);
+		});
+		
+	});
+
+	describe('moving a stroke', () => {
+		
+		it('does not change the number of scenes', () => {
+			let movableStrokes = [{
+				points: [point(10,10), point(11,11), point(12,12)]
+			}];
+			let result = scenes(
+				[
+					{ strokes: [] },
+					{ strokes: movableStrokes }
+				],
+				updatePosition(movableStrokes, 5, 15, 1)
+			);
+			expect(result).to.have.length(2);
 		});
 		
 	});
@@ -70,7 +97,9 @@ describe('scenes', () => {
 
 		it('to a scene does not create a new one', () => {
 			let result = scenes(
-				[{ strokes:[] }],
+				[
+					{ strokes:[] }
+				],
 				appendPoint(10,10, undefined, 0)
 			);
 			expect(result).to.have.length(1);
