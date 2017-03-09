db68c3b07a305bf7e8e009e6697c9a1f0445d1a3
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-03-09 19:17:11 +0100

    remove a createClass from tests #52

--- test/components/smart/ModuleChooser.spec.jsx
+++ test/components/smart/ModuleChooser.spec.jsx
@@ -5,28 +5,26 @@ import { point } from '../../helpers';
 
 const MockedSubComponent1 = () => <canvas />;
 const MockedSubComponent2 = () => <span />;
+const MockedComponent = ModuleChooser([MockedSubComponent1, MockedSubComponent2]);
+const TestParentPrototype1 = class TestParentPrototype1 extends React.Component {
+	render() {
+		return (<MockedComponent
+			componentIndex={this.state ? this.state.componentIndex : 0}
+			bounds={{
+				width: 100,
+				height: 50,
+				x: 10,
+				y: 10,
+			}}
+			strokes={[{ points: [point(10, 10), point(11, 11)] }]}
+		/>);
+	}
+};
 
 describe('ModuleChooser', () => {
 	describe('enabling ploma', () => {
 		it('is possible when at the same time changing strokes', () => {
-			const MockedComponent = ModuleChooser([MockedSubComponent1, MockedSubComponent2]);
-			const TestParent = React.createFactory(React.createClass({
-				getInitialState() {
-					return { componentIndex: 0 };
-				},
-				render() {
-					return (<MockedComponent
-						componentIndex={this.state.componentIndex}
-						bounds={{
-							width: 100,
-							height: 50,
-							x: 10,
-							y: 10,
-						}}
-						strokes={[{ points: [point(10, 10), point(11, 11)] }]}
-					/>);
-				},
-			}));
+			const TestParent = React.createFactory(TestParentPrototype1);
 			const parent = TestUtils.renderIntoDocument(TestParent());
 			expect(TestUtils.scryRenderedDOMComponentsWithTag(parent, 'canvas')).to.have.length(1);
 			parent.setState({
@@ -36,25 +34,11 @@ describe('ModuleChooser', () => {
 		});
 
 		it('Always returns some Component', () => {
-			const MockedComponent = ModuleChooser([MockedSubComponent1]);
-			const TestParent = React.createFactory(React.createClass({
-				getInitialState() {
-					return { componentIndex: 0 };
-				},
-				render() {
-					return (<MockedComponent
-						componentIndex={1}
-						bounds={{
-							width: 100,
-							height: 50,
-							x: 10,
-							y: 10,
-						}}
-						strokes={[{ points: [point(10, 10), point(11, 11)] }]}
-					/>);
-				},
-			}));
+			const TestParent = React.createFactory(TestParentPrototype1);
 			const parent = TestUtils.renderIntoDocument(TestParent());
+			parent.setState({
+				componentIndex: 2,
+			});
 			expect(TestUtils.scryRenderedDOMComponentsWithTag(parent, 'canvas')).to.have.length(0);
 			expect(TestUtils.scryRenderedDOMComponentsWithTag(parent, 'span')).to.have.length(0);
 			expect(TestUtils.scryRenderedDOMComponentsWithTag(parent, 'div')).to.have.length(1);
