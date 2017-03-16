7a30d58fad15fae0566764065cff08017afb7a6a
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-03-16 15:20:10 +0100

    Do not filter states for relevance in scenes to fix performance lag while drawing #59

--- src/client/app/helpers/relevantStatesForScene.js
+++ src/client/app/helpers/relevantStatesForScene.js
@@ -4,14 +4,15 @@ import filter from 'lodash/filter';
 import { type Scene } from '../typeDefinitions';
 
 export default function (states: Array<Scene>, sceneIndex: number) {
-	return filter(states, (state, index) => {
-		if (index === 0) {
-			return states[index][sceneIndex] &&
-				(states[index][sceneIndex].strokes ?
-					states[index][sceneIndex].strokes.length > 0 :
-					true);
-		}
-		return states[index][sceneIndex] &&
-			!isEqual(states[index][sceneIndex], states[index - 1][sceneIndex]);
-	});
+	return states;
+	// return filter(states, (state, index) => {
+	// 	if (index === 0) {
+	// 		return states[index][sceneIndex] &&
+	// 			(states[index][sceneIndex].strokes ?
+	// 				states[index][sceneIndex].strokes.length > 0 :
+	// 				true);
+	// 	}
+	// 	return states[index][sceneIndex] &&
+	// 		!isEqual(states[index][sceneIndex], states[index - 1][sceneIndex]);
+	// });
 }
