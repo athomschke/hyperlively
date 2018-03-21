3fdf7a92c53be32e5941b1bae95ad00e6eb0dde4
Astrid Thomschke <astrid.thomschke@gmail.com>
2018-03-21 10:31:06 +0100

    Use immer js to reduce interpretation results #62

--- src/client/app/reducers/interpretation.js
+++ src/client/app/reducers/interpretation.js
@@ -1,4 +1,7 @@
 // @flow
+/* eslint-disable no-param-reassign, consistent-return, default-case */
+import produce from 'immer';
+
 import { TOGGLE_INTERPRETER, APPEND_POINT, APPEND_STROKE, RECEIVE_TEXT_CANDIDATES, RECEIVE_SHAPE_CANDIDATES } from 'src/client/app/constants/actionTypes';
 import { type TOGGLE_INTERPRETER_ACTION, RECOGNIZE_HANDWRITING_ACTION, APPEND_POINT_ACTION, APPEND_STROKE_ACTION } from 'src/client/app/actionTypeDefinitions';
 import { type InterpretationState } from 'src/client/app/typeDefinitions';
@@ -11,40 +14,31 @@ const initialState = () => ({
 	},
 });
 
-function interpretation(
-		state: InterpretationState = initialState(),
-		action: TOGGLE_INTERPRETER_ACTION | RECOGNIZE_HANDWRITING_ACTION |
-			APPEND_POINT_ACTION | APPEND_STROKE_ACTION) {
+type Action = TOGGLE_INTERPRETER_ACTION | RECOGNIZE_HANDWRITING_ACTION |
+	APPEND_POINT_ACTION | APPEND_STROKE_ACTION
+
+const interpretation =
+(state: InterpretationState = initialState(), action: Action) =>
+produce(state, (draftState) => {
 	switch (action.type) {
 	case TOGGLE_INTERPRETER: {
-		return Object.assign({}, state, {
-			showInterpreter: action.boolean,
-		});
+		draftState.showInterpreter = action.boolean;
+		break;
 	}
 	case RECEIVE_TEXT_CANDIDATES: {
-		return Object.assign({}, state, {
-			interpretations: Object.assign({}, state.interpretations, {
-				text: action.candidates,
-			}),
-		});
+		draftState.interpretations.text = action.candidates;
+		break;
 	}
 	case RECEIVE_SHAPE_CANDIDATES: {
-		return Object.assign({}, state, {
-			interpretations: Object.assign({}, state.interpretations, {
-				shape: action.candidates,
-			}),
-		});
+		draftState.interpretations.shape = action.candidates;
+		break;
 	}
 	case APPEND_POINT:
 	case APPEND_STROKE: {
-		return Object.assign({}, state, {
-			interpretations: initialState().interpretations,
-		});
-	}
-	default: {
-		return state;
+		draftState.interpretations = initialState().interpretations;
+		break;
 	}
 	}
-}
+});
 
 export { interpretation };
