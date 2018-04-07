cbf79b4c24247c93ebc5d55f595804033494b466
Astrid Thomschke <astrid.thomschke@gmail.com>
2018-04-07 14:33:26 +0200

    use immerjs in scenes reducer #62

--- src/client/app/reducers/content/scenes.js
+++ src/client/app/reducers/content/scenes.js
@@ -1,4 +1,6 @@
 // @flow
+import produce from 'immer';
+
 import { ADD_SCENE, ADD_SCENE_AT } from 'src/client/app/constants/actionTypes';
 import type { Scene } from 'src/client/app/typeDefinitions';
 import type { ADD_SCENE_AT_ACTION, ADD_SCENE_ACTION } from 'src/client/app/actionTypeDefinitions';
@@ -7,29 +9,20 @@ import { scene, sceneActionTypes, type SceneActionType } from './scene';
 
 type ScenesActionType = SceneActionType | ADD_SCENE_AT_ACTION | ADD_SCENE_ACTION
 
-function scenes(state: Array<Scene> = [], action: ScenesActionType) {
+const scenes = (state: Array<Scene> = [], action: ScenesActionType) =>
+produce(state, (draftState) => {
 	switch (action.type) {
 	case ADD_SCENE:
-		return [
-			...state,
-			scene(undefined, action),
-		];
+		draftState.push(scene(undefined, action));
+		break;
 	case ADD_SCENE_AT:
-		return [
-			...state.slice(0, action.number),
-			scene(undefined, action),
-			...state.slice(action.number, state.length),
-		];
+		draftState.splice(action.number, 0, scene(undefined, action));
+		break;
 	default:
 		if (sceneActionTypes.includes(action.type)) {
-			return [
-				...state.slice(0, action.sceneIndex),
-				scene(state[action.sceneIndex], action),
-				...state.slice(action.sceneIndex + 1, state.length),
-			];
+			draftState.splice(action.sceneIndex, 1, scene(state[action.sceneIndex], action));
 		}
-		return state;
 	}
-}
+});
 
 export { scenes };
