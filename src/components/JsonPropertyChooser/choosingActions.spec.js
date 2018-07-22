cd0b381ff1cf05f6b341ade7e666f34aab4e2f09
Astrid Thomschke <astrid.thomschke@gmail.com>
2018-07-22 13:07:32 +0200

    splitting tests for json property chooser and data object constructor

---
+++ src/components/JsonPropertyChooser/choosingActions.spec.js
@@ -0,0 +1,105 @@
+// @flow
+import { expect } from 'chai';
+import { flatten, map, forEach } from 'lodash';
+
+import type { ReactTreeLeafFormat, ReactTreeNodeFormat } from 'src/types';
+
+import { formatObject } from './choosingActions';
+
+const flattenedTree = (root) => {
+	let items = [root];
+	forEach(root.children, (child) => {
+		items = items.concat(flattenedTree(child));
+	});
+	return items;
+};
+
+const getItemsFromDataArray = dataArray => flatten(map(dataArray, item => flattenedTree(item)));
+
+const exampleChecks = [['a', 'a2'], ['b']];
+
+const exampleCollapses = [['a']];
+
+const exampleTree = {
+	a: {
+		a1: 'a1',
+		a2: 'a2',
+	},
+	b: 'b',
+	c: 'c',
+};
+
+const exampleArray = [
+	{
+		label: 'a',
+		key: 'a',
+		checkbox: true,
+		checked: false,
+		collapsed: false,
+		collapsible: true,
+		children: [
+			{
+				label: 'a1: a1',
+				key: 'a1',
+				checkbox: true,
+				checked: false,
+			},
+			{
+				label: 'a2: a2 (property 0)',
+				key: 'a2',
+				checkbox: true,
+				checked: true,
+			},
+		],
+	},
+	{
+		label: 'b: b (property 1)',
+		key: 'b',
+		checkbox: true,
+		checked: true,
+	},
+	{
+		label: 'c: c',
+		key: 'c',
+		checkbox: true,
+		checked: false,
+	},
+];
+
+describe('Formatting the JSONPropertyChooser Data object', () => {
+	it('formats the json tree for the tree view menu', () => {
+		const gottenArray = formatObject(
+			exampleTree, exampleChecks, [], exampleChecks, 0,
+		);
+		const wantedArray = exampleArray;
+		expect(gottenArray).to.deep.equal(wantedArray);
+	});
+
+	it('checks the chosen checkmarks', () => {
+		const formattedTree: Array<ReactTreeLeafFormat | ReactTreeNodeFormat> = formatObject(
+			exampleTree, exampleChecks, [], exampleChecks, 0,
+		);
+		expect((formattedTree[0]:any).children[1].checked).to.be.true();
+	});
+
+	it('collapses collapsed nodes', () => {
+		const formattedTree = formatObject(
+			exampleTree, exampleChecks, exampleCollapses, exampleChecks, 0,
+		);
+		expect((formattedTree[0]:any).collapsed).to.be.true();
+	});
+
+	it('renders an entry for each top level handwriting recognition result plus one for the last strokes plus the selected strokes', () => {
+		const formattedTree = formatObject(exampleTree, [], [], [], 0);
+		const items = getItemsFromDataArray(formattedTree);
+		expect(items.filter(item => item.label)).to.have.length(5);
+	});
+
+	it('renders a checkbox for each handwriting recognition result, nodes as well as leafes', () => {
+		const formattedTree = formatObject(
+			exampleTree, exampleChecks, exampleCollapses, exampleChecks, 0,
+		);
+		const items = getItemsFromDataArray(formattedTree);
+		expect(items.filter(item => item.checked)).to.have.length(2);
+	});
+});
--- src/components/JsonPropertyChooser/index.spec.jsx
+++ src/components/JsonPropertyChooser/index.spec.jsx
@@ -3,24 +3,13 @@ import { expect } from 'chai';
 import React from 'react';
 import { shallow } from 'enzyme';
 import Tree from 'rc-tree';
-import {
-	flatten, map, forEach, filter,
-} from 'lodash';
 import { spy } from 'sinon';
-
-import type { ReactTreeLeafFormat, ReactTreeNodeFormat } from 'src/types';
-
-import { formatObject } from './choosingActions';
 
 import JsonPropertyChooser, { type JsonPropertyChooserProps } from '.';
 
 const renderWithProps = (props: JsonPropertyChooserProps) => shallow(<JsonPropertyChooser {...props} />);
 
 const shallowWithProps = (props: JsonPropertyChooserProps) => shallow(<JsonPropertyChooser {...props} />);
-
-const exampleChecks = [['a', 'a2'], ['b']];
-
-const exampleCollapses = [['a']];
 
 const exampleTree = {
 	a: {
@@ -35,127 +24,24 @@ const defaultProps = (): JsonPropertyChooserProps => ({
 	onParameterChoose: () => undefined,
 	onCheckedPathsChange: () => undefined,
 	onCollapsedPathsChange: () => undefined,
+	isOpen: true,
 	checkedPaths: [],
 	collapsedPaths: [],
 	jsonTree: {},
 });
 
-const exampleLastStrokes = [{
-	d: 'd',
-}];
-
-const exampleSelectedStrokes = [{
-	e: 'e',
-}];
-
-const exampleArray = [
-	{
-		label: 'a',
-		key: 'a',
-		checkbox: true,
-		checked: false,
-		collapsed: false,
-		collapsible: true,
-		children: [
-			{
-				label: 'a1: a1',
-				key: 'a1',
-				checkbox: true,
-				checked: false,
-			},
-			{
-				label: 'a2: a2 (property 0)',
-				key: 'a2',
-				checkbox: true,
-				checked: true,
-			},
-		],
-	},
-	{
-		label: 'b: b (property 1)',
-		key: 'b',
-		checkbox: true,
-		checked: true,
-	},
-	{
-		label: 'c: c',
-		key: 'c',
-		checkbox: true,
-		checked: false,
-	},
-];
-
-const flattenedTree = (root) => {
-	let items = [root];
-	forEach(root.children, (child) => {
-		items = items.concat(flattenedTree(child));
-	});
-	return items;
-};
-
-const getItemsFromDataArray = dataArray => flatten(map(dataArray, item => flattenedTree(item)));
-
-describe('JsonProperty Chooser', () => {
+describe('JSONProperty Chooser', () => {
 	describe('showing the parameter chooser', () => {
-		let parameterChooser;
-
-		beforeEach(() => {
-			parameterChooser = shallowWithProps({
-				...defaultProps(),
-				isOpen: true,
-				jsonTree: Object.assign({}, exampleTree, {
-					lastStrokes: exampleLastStrokes,
-				}, {
-					selectedStrokes: exampleSelectedStrokes,
-				}),
-			});
-		});
-
 		it('renders a tree menu', () => {
-			expect(parameterChooser.find(JsonPropertyChooser)).to.exist();
-		});
-
-		it('renders an entry for each top level handwriting recognition result plus one for the last strokes plus the selected strokes', () => {
-			const items = getItemsFromDataArray(parameterChooser.find(Tree).props().data);
-			expect(filter(items, 'label')).to.have.length(11);
-		});
-
-		it('renders a checkbox for each handwriting recognition result, nodes as well as leafes', () => {
-			const items = getItemsFromDataArray(parameterChooser.find(Tree).props().data);
-			expect(filter(items, 'checkbox')).to.have.length(11);
-		});
-
-		it('formats the json tree for the tree view menu', () => {
-			const gottenArray = formatObject(
-				exampleTree, exampleChecks, [], exampleChecks, 0,
-			);
-			const wantedArray = exampleArray;
-			expect(gottenArray).to.deep.equal(wantedArray);
-		});
-
-		it('checks the chosen checkmarks', () => {
-			const formattedTree: Array<ReactTreeLeafFormat | ReactTreeNodeFormat> = formatObject(
-				exampleTree, exampleChecks, [], exampleChecks, 0,
-			);
-			expect((formattedTree[0]:any).children[1].checked).to.be.true();
-		});
-
-
-		it('collapses collapsed nodes', () => {
-			const formattedTree = formatObject(
-				exampleTree, exampleChecks, exampleCollapses, exampleChecks, 0,
-			);
-			expect((formattedTree[0]:any).collapsed).to.be.true();
+			expect(shallowWithProps(defaultProps()).find(Tree)).to.have.length(1);
 		});
 	});
 
 	describe('Collapsing a json property that is an object', () => {
-		const onCheckedPathsChange = spy();
-		const onCollapsedPathsChange = spy();
-		let parameterChooser;
-
-		beforeEach(() => {
-			parameterChooser = shallowWithProps({
+		it('performs the callback', () => {
+			const onCheckedPathsChange = spy();
+			const onCollapsedPathsChange = spy();
+			const parameterChooser = shallowWithProps({
 				jsonTree: exampleTree,
 				checkedPaths: [],
 				collapsedPaths: [],
@@ -163,9 +49,6 @@ describe('JsonProperty Chooser', () => {
 				onCheckedPathsChange,
 				onCollapsedPathsChange,
 			});
-		});
-
-		it('performs the callback', () => {
 			const treeMenu = parameterChooser.find(Tree);
 			treeMenu.prop('onTreeNodeCollapseChange')([0]);
 			expect(onCollapsedPathsChange.args[0][0]).to.deep.equal([['a']]);
@@ -173,12 +56,11 @@ describe('JsonProperty Chooser', () => {
 	});
 
 	describe('Expanding a json property that is an object', () => {
-		let parameterChooser;
-		const onCheckedPathsChange = spy();
-		const onCollapsedPathsChange = spy();
+		it('performs the callback', () => {
+			const onCheckedPathsChange = spy();
+			const onCollapsedPathsChange = spy();
 
-		beforeEach(() => {
-			parameterChooser = renderWithProps({
+			const parameterChooser = renderWithProps({
 				jsonTree: exampleTree,
 				checkedPaths: [],
 				collapsedPaths: [['a']],
@@ -186,9 +68,6 @@ describe('JsonProperty Chooser', () => {
 				onCheckedPathsChange,
 				onCollapsedPathsChange,
 			});
-		});
-
-		it('performs the callback', () => {
 			const treeMenu = parameterChooser.find(Tree);
 			treeMenu.prop('onTreeNodeCollapseChange')([0]);
 			expect(onCollapsedPathsChange.args[0][0]).to.deep.equal([]);
